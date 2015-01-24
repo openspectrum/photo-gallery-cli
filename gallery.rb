b6fbbfd8adfd7db9b127e8d3b1d1ea10cecff2cb
Tanner Welsh <tannerwelsh@gmail.com>
2015-01-24 16:50:45 -0500

    img_tag method returns an HTML <img> tag with provided src; solves 0.2

--- gallery.rb
+++ gallery.rb
@@ -2,12 +2,21 @@ def absolute_path(file)
   File.absolute_path(file)
 end
 
+def img_tag(photo_path)
+  img_tag_str = "<img src=\"#{photo_path}\">"
+
+  return img_tag_str
+end
+
 def run_test
-  p absolute_path("photos/bunny-1.jpg") == "/Users/tanner/Clients/code-union/cohort-web-fun-2015-01-19/photo-gallery-cli/photos/bunny-1.jpg"
+  photo_file = "photos/bunny-1.jpg"
+
+  p absolute_path(photo_file) == "/Users/tanner/Clients/code-union/cohort-web-fun-2015-01-19/photo-gallery-cli/photos/bunny-1.jpg"
+  p img_tag( absolute_path(photo_file) ) == "<img src=\"/Users/tanner/Clients/code-union/cohort-web-fun-2015-01-19/photo-gallery-cli/photos/bunny-1.jpg\">"
 end
 
 # run_test
 
 photo_file = ARGV[0]
 
-puts absolute_path(photo_file)
+puts img_tag( absolute_path(photo_file) )
