3a6650388f7c8c5d8126412c2a60fd73b0880f16
Tanner Welsh <tannerwelsh@gmail.com>
2015-01-25 19:00:43 -0500

    Handle multiple photo files for input; solves 0.4

--- gallery.rb
+++ gallery.rb
@@ -8,8 +8,8 @@ def img_tag(photo_path)
   return img_tag_str
 end
 
-def html_gallery(image_tag)
-  full_html = <<-HTML
+def html_gallery(image_tags)
+  html_top = <<-HTML
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,31 +17,46 @@ def html_gallery(image_tag)
 </head>
 <body>
   <h1>My Gallery</h1>
-  #{image_tag}
+  HTML
+
+  images_html = ""
+
+  image_tags.each do |tag|
+    images_html += tag
+  end
+
+  html_bottom = <<-HTML
 </body>
 </html>
   HTML
 
-  return full_html
+  return html_top + images_html + html_bottom
 end
 
 def run_test
   photo_file = "photos/bunny-1.jpg"
 
   valid_img_html = "<img src=\"/Users/tanner/Clients/code-union/cohort-web-fun-2015-01-19/photo-gallery-cli/photos/bunny-1.jpg\">"
+  valid_img_html_2 = "<img src=\"/Users/tanner/Clients/code-union/cohort-web-fun-2015-01-19/photo-gallery-cli/photos/bunny-2.jpg\">"
 
   p absolute_path(photo_file) == "/Users/tanner/Clients/code-union/cohort-web-fun-2015-01-19/photo-gallery-cli/photos/bunny-1.jpg"
   p img_tag( absolute_path(photo_file) ) == valid_img_html
-  p html_gallery(valid_img_html).include?(valid_img_html)
+  puts html_gallery( [valid_img_html, valid_img_html_2] ).include?(valid_img_html)
 end
 
 # run_test
 
 if __FILE__ == $PROGRAM_NAME
-  photo_file = ARGV[0]
-  abs_photo_path = absolute_path(photo_file)
-  image_tag = img_tag(abs_photo_path)
-  full_page_html = html_gallery(image_tag)
+  all_the_images = []
+
+  ARGV.each do |file|
+    abs_filename = absolute_path(file)
+    image_tag = img_tag(abs_filename)
+
+    all_the_images.push(image_tag)
+  end
+
+  full_page_html = html_gallery(all_the_images)
 
   puts full_page_html
 end
